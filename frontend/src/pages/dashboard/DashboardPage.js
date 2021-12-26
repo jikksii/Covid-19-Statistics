909955664fb85f89a79bf34d1f7e62ff1507e609
jikksii <giorgiijikia@gmail.com>
2021-12-26 23:00:10 +0400

    Create list for cards.

--- frontend/src/pages/dashboard/DashboardPage.js
+++ frontend/src/pages/dashboard/DashboardPage.js
@@ -1,9 +1,29 @@
+import Card from '../../components/Card/Card';
 import Table from '../../components/Table/Table';
 import styles from './DashboardPage.module.css'
 const DashboardPage = () =>{
     return <div className={styles.dashboard}>
         <Table/>
-        <div className='cards'>Cards</div>
+        <div className={styles['card-list']}>
+            <Card>
+                <h2 className={styles['card-title']}>Death</h2>
+                <div className={styles['card-number']}>
+                    25596
+                </div>
+            </Card>
+            <Card>
+                <h2 className={styles['card-title']}>Recoveries</h2>
+                <div className={styles['card-number']}>
+                    25596
+                </div>
+            </Card>
+            <Card>
+                <h2 className={styles['card-title']}>Confirmeds</h2>
+                <div className={styles['card-number']}>
+                    25596
+                </div>
+            </Card>
+        </div>
     </div>
 }
 export default DashboardPage;
