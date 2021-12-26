84f2e4ce0de46b2f327e97ac0b6ae8f4f37d7de2
jikksii <giorgiijikia@gmail.com>
2021-12-26 21:21:47 +0400

    Add debounce to table search input.

--- frontend/src/components/Table/TableHeader/TableSearch.js
+++ frontend/src/components/Table/TableHeader/TableSearch.js
@@ -1,9 +1,20 @@
 import styles from './TableSearch.module.css'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from 'react'
 const TableSearch = ()=>{
+    const [searchTerm,setSearchTerm] = useState('');
+
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            console.log(searchTerm)
+        }, 500)
+        return () => clearTimeout(delayDebounceFn)
+    },[searchTerm])
+
+
     return <div className={styles.search}>
-        <input className={styles.input} type={"text"} placeholder='Search'/>
+        <input className={styles.input} type={"text"} placeholder='Search' onChange={(event) => setSearchTerm(event.target.value)}/>
         <div><FontAwesomeIcon icon={faSearch}/></div>
     </div>
 }
