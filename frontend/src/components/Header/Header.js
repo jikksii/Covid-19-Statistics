424ed44834b86c1e2d2b008554dfc156292d9882
jikksii <giorgiijikia@gmail.com>
2021-12-26 17:09:53 +0400

    Add css styles to header.

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,4 +1,12 @@
+import styles from './Header.module.css'
 const Header = () =>{
-    return <div>Header</div>
+    return <header className={styles.header}>
+        <div className={styles['text-box']}>
+            <h1 className={styles['heading-primary']}>
+                <span className={styles['heading-primary-main']}>Covid 19</span>
+                <span className={styles['heading-primary-sub']}>Statistics</span>
+            </h1>
+        </div>
+    </header>
 }
 export default Header;
