3845556d890e1f585deb40be997ea24944762ccb
jikksii <giorgiijikia@gmail.com>
2021-12-26 23:26:31 +0400

    Add logout view to mainLayout.

--- frontend/src/layouts/MainLayout/MainLayout.js
+++ frontend/src/layouts/MainLayout/MainLayout.js
@@ -3,6 +3,9 @@ import Header from "../../components/Header/Header";
 import Spinner from "../../components/Spinner/Spinner";
 import useHttp from "../../hooks/use-http";
 import styles from './MainLayout.module.css'
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 const MainLayout = ({children}) => {
     
     const handleFetchTestData = data =>{
@@ -18,9 +21,17 @@ const MainLayout = ({children}) => {
         getTestData()
     },[])
 
+    const handleLogOut = () => {
+        console.log("Log out");
+    }
+
     return (
         <div className={`${styles.MainLayout} ${isFetchingTestData && styles.Loading}` }>
             {isFetchingTestData && <Spinner/>}
+            {!isFetchingTestData && 
+            <div className={styles.controller}>
+                <FontAwesomeIcon className={styles['controller-auth']} icon={faSignOutAlt} onClick={handleLogOut} />
+            </div>}
             {!isFetchingTestData &&<Header />}
             {!isFetchingTestData && children}
         </div>
