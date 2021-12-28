a2979a5cc32deb13c023499ef826c4e6eef0c7ac
jikksii <giorgiijikia@gmail.com>
2021-12-28 15:47:01 +0400

    Add Authentication routes to api.

---
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        
+    }
+
+
+    public function logIn(Request $request){
+
+    }
+
+    public function logOut(Request $request){
+
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -29,6 +30,9 @@ Route::post('/test',function(){
     ];
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login',[AuthController::class,'logIn']);
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/register',[AuthController::class,'register']);
+    Route::put('/logout',[AuthController::class,'logOut']);
 });
